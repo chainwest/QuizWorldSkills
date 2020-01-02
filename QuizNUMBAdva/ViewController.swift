6045dbd96c73219b97e3cab224c4f066744b4ab0
endlessresonance <evgeniyurbanovskiy@gmail.com>
2020-01-02 12:48:07 +0700

    Initial Commit

---
+++ QuizNUMBAdva/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizNUMBAdva
+//
+//  Created by Evgeniy on 02.01.2020.
+//  Copyright © 2020 Evgeniy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
